2ab78e7d26146f5ea6bb1cd00fe215b31a2344b6
JayyyyLee <97768904+JayyyyLee@users.noreply.github.com>
2022-08-19 15:54:40 -0700

    update HUD

---
+++ G1_Menu/HUD.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+signal newGame
+signal assessment
+
+func show_message(text):
+	$title.text = text
+	$title.show()
+	$messageTimer.start()
+
+func _on_newGame_pressed():
+	$newGame.hide()
+	$assessment.hide()
+	emit_signal("newGame")
+	
+func _on_assessment_pressed():
+	$assessment.hide()
+	$newGame.hide()
+	emit_signal("assessment")
+
+func _on_messageTimer_timeout():
+	$title.hide()
+
+func show_game_over():
+	show_message("Finished!")
+	yield($messageTimer, "timeout")
+	$title.text = "TITLE"
+	$title.show()
+	yield(get_tree().create_timer(1), "timeout")
+	$newGame.show()
+	$assessment.show()
