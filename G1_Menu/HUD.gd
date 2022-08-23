5c538beeadd43ab9c54c4e55f5eaa48b189bad70
JayyyyLee <97768904+JayyyyLee@users.noreply.github.com>
2022-08-23 11:52:45 -0700

    HUD update

--- G1_Menu/HUD.gd
+++ G1_Menu/HUD.gd
@@ -1,7 +1,9 @@
 extends CanvasLayer
 
 signal newGame
-signal assessment
+signal image
+signal audio
+signal random
 
 func show_message(text):
 	$title.text = text
@@ -11,12 +13,17 @@ func show_message(text):
 func _on_newGame_pressed():
 	$newGame.hide()
 	$assessment.hide()
+	$title.hide()
 	emit_signal("newGame")
 	
 func _on_assessment_pressed():
 	$assessment.hide()
 	$newGame.hide()
-	emit_signal("assessment")
+	$title.hide()
+	$prompt.show()
+	$audio.show()
+	$random.show()
+	$image.show()
 
 func _on_messageTimer_timeout():
 	$title.hide()
@@ -29,3 +36,25 @@ func show_game_over():
 	yield(get_tree().create_timer(1), "timeout")
 	$newGame.show()
 	$assessment.show()
+
+func _on_image_pressed():
+	$image.hide()
+	$audio.hide()
+	$random.hide()
+	$prompt.hide()
+	emit_signal("image")
+
+func _on_audio_pressed():
+	$image.hide()
+	$audio.hide()
+	$random.hide()
+	$prompt.hide()
+	emit_signal("audio")
+	
+func _on_random_pressed():
+	$image.hide()
+	$audio.hide()
+	$random.hide()
+	$prompt.hide()
+	emit_signal("random")
+
