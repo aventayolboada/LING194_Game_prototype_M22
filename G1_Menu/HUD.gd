b922a12fa1a5db98fc6806645f57f989d0e6433f
Summer Xia <summeryuqing217@gmail.com>
2022-08-30 21:58:29 -0700

    menu change (background added)

--- G1_Menu/HUD.gd
+++ G1_Menu/HUD.gd
@@ -6,27 +6,25 @@ signal assessment
 func show_message(text):
 	$title.text = text
 	$title.show()
-	$messageTimer.start()
 
 func _on_newGame_pressed():
-	get_tree(). change_scene("res://G4_Typing/Main.tscn")
+	$Clicksound.play()
 	$newGame.hide()
 	$assessment.hide()
 	$title.hide()
 	emit_signal("newGame")
 	
 func _on_assessment_pressed():
-	get_tree(). change_scene("res://G5_Assessment/Typing Segment Score Screen/TypingCompleteScreen.tscn")
 	$assessment.hide()
 	$newGame.hide()
 	$title.hide()
 	emit_signal("assessment")
+	$Clicksound.play()
 
-func _on_messageTimer_timeout():
-	$title.hide()
 
 func show_game_over():
 	show_message("Finished!")
+	$Gameoversound.play()
 	yield($messageTimer, "timeout")
 	$title.text = "TITLE"
 	$title.show()
